05efb7d3ed049ebf435760d9baecef0624be48c4
Andreas <a.garoufalias@gmail.com>
2025-05-27 08:49:55 -0400

    Adding page under construction note

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,9 @@ export default function HomePage() {
       {/* Navbar */}
       <nav className="lg:w-full md:w-full w-full px-8 py-6 flex justify-between items-center shadow-md fixed">
         <Link href="/">
-          <h1 className="text-2xl font-bold text-indigo-600">Synitia</h1>
+          <h1 className="text-2xl font-bold text-indigo-600">
+            Synitia (Pagina bajo Construccion)
+          </h1>
         </Link>
         <div className="space-x-6">
           <a
